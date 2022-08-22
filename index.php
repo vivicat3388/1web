4649b921795f60a396fde3f755ff1b6b1f7c6250
vivicat3388 <111785019+vivicat3388@users.noreply.github.com>
2022-08-22 22:50:58 +0100

    Displays Gundam Unicorn

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
+<script header('Content-Type: image/jpeg');> </script>
+<script $image = 'path/to/image.jpg';> </script>
+<script readfile($image);></script>
+
 <html>
  <head>
- <title>Gundam Unicorn</title>
- </head>
- <body>
-	<?php
- 
+  <title>display image</title>
+   </head>
+    <body>
+     <p>Unicorn Gundam</p>
+     	<?php
 	echo "<img src='gundam-unicorn-001.jpg' >"; 
 
 	?>  
  </body>
  </html>
+
